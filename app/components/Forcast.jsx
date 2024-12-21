9b00158ded56a49ea509d44c6785905a49f414a6
rautkaveri24 <rautkaveri24@gmail.com>
2024-12-21 21:43:29 +0530

    forest component bg changes

--- app/components/Forcast.jsx
+++ app/components/Forcast.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Forcast = () => {
   return (
-    <div className='w-[100%] h-[100%] flex flex-col items-center justify-center gap-6'>
+    <div className='w-[100%] h-[100%] flex flex-col items-center justify-center gap-6 absolute z-20'>
         <div className='w-[212px] h-[30px] flex gap-2 items-center'>
             <img className='w-3 h-3' src="./message.png" alt="" />
             <p className=' text-xs font-semibold text-[#CCFBEF]'>FORECASTS</p>
--- app/components/Glance.jsx
+++ app/components/Glance.jsx
@@ -43,9 +43,16 @@ const Glance = () => {
             <div className="w-[260px] h-[455px] rounded-[20px] flex flex-col items-center justify-center gap-6"style={{boxShadow: "0px 5px 22px 0px #0000000A, 0px 0px 0px 1px #0000000F"}}>
                 <BarChart/>
             </div>
-            <div className={` ${styles.linerGradiant} w-[260px] h-[455px] rounded-[20px]`}style={{boxShadow: "0px 5px 22px 0px #0000000A, 0px 0px 0px 1px #0000000F"}}>
+            <div className={` ${styles.linerGradiant} w-[260px] h-[455px] rounded-[20px] relative`}style={{boxShadow: "0px 5px 22px 0px #0000000A, 0px 0px 0px 1px #0000000F"}}>
+            
               <Forcast/>
+              <div className="w-full h-full overflow-hidden bg-[#109c89] relative rounded-[20px]">
+        <div className="bg-gradient-to-br from-[#2db19f] to-[#30ae9c] absolute w-[800px] aspect-square rounded-full bottom-[30%] -right-1/3"></div>
+        <div className="bg-gradient-to-br from-[#2eb7a3] to-[#39b8a7] absolute w-[400px] aspect-square rounded-full bottom-[50%] right-[20%]"></div>
+        <div className="bg-gradient-to-br from-[#15b69e] to-[#25b6a1] absolute w-[200px] aspect-square rounded-full -top-[15%] -left-[25%]"></div>
+      </div>
             </div>
+            
           </div>
           </div>
           <div className="w-[100%] h-[86px] flex justify-between items-center">
